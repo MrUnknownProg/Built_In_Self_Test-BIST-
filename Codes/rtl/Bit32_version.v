bb1441331af889fc38035d472b96fe5f92166726
Rutuj Rotkar <147727459+MrUnknownProg@users.noreply.github.com>
2026-09-01 00:23:03 +0530

    Add files via upload

---
+++ Codes/rtl/Bit32_version.v
@@ -0,0 +1,147 @@
+//Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+//Copyright 2022-2025 Advanced Micro Devices, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2025.2 (win64) Build 6299465 Fri Nov 14 19:35:11 GMT 2025
+//Date        : Mon Aug 31 23:53:25 2026
+//Host        : HP15S running 64-bit major release  (build 9200)
+//Command     : generate_target Bit32_version.bd
+//Design      : Bit32_version
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+(* CORE_GENERATION_INFO = "Bit32_version,IP_Integrator,{x_ipVendor=xilinx.com,x_ipLibrary=BlockDiagram,x_ipName=Bit32_version,x_ipVersion=1.00.a,x_ipLanguage=VERILOG,numBlks=9,numReposBlks=9,numNonXlnxBlks=0,numHierBlks=0,maxHierDepth=0,numSysgenBlks=0,numHlsBlks=0,numHdlrefBlks=7,numPkgbdBlks=0,bdsource=USER,synth_mode=Hierarchical}" *) (* HW_HANDOFF = "Bit32_version.hwdef" *) 
+module Bit32_version
+   (clk_i,
+    done_o_0,
+    fail_count_o_0,
+    fail_flag_o_0,
+    fault_addr_o,
+    fault_addr_valid_o,
+    fault_en_i_0,
+    repair_count_o_0,
+    repair_full_o_0,
+    repair_reset_i_0,
+    repaired_logical_addr_o_0,
+    repaired_physical_addr_o_0,
+    repaired_valid_o_0,
+    rst_i,
+    start_i_0);
+  input clk_i;
+  output done_o_0;
+  output [7:0]fail_count_o_0;
+  output fail_flag_o_0;
+  output [9:0]fault_addr_o;
+  output fault_addr_valid_o;
+  input fault_en_i_0;
+  output [9:0]repair_count_o_0;
+  output repair_full_o_0;
+  input repair_reset_i_0;
+  output [9:0]repaired_logical_addr_o_0;
+  output [9:0]repaired_physical_addr_o_0;
+  output repaired_valid_o_0;
+  input rst_i;
+  input start_i_0;
+
+  wire addr_gen_0_addr_done_o;
+  wire [9:0]addr_gen_0_addr_o;
+  wire [9:0]address_remapper_0_physical_addr_o;
+  wire [31:0]blk_mem_gen_0_doutb;
+  wire clk_i;
+  wire control_logic_0_addr_dir_o;
+  wire control_logic_0_addr_en_o;
+  wire control_logic_0_addr_rst_o;
+  wire control_logic_0_compare_en_o;
+  wire [1:0]control_logic_0_pattern_sel_o;
+  wire control_logic_0_we_o;
+  wire [31:0]data_gen_0_data_o;
+  wire done_o_0;
+  wire [7:0]fail_count_o_0;
+  wire fail_flag_o_0;
+  wire [9:0]fault_addr_o;
+  wire fault_addr_valid_o;
+  wire fault_en_i_0;
+  wire [31:0]fault_inject_0_data_o;
+  wire [9:0]repair_controller_0_lookup_phys_addr_o;
+  wire repair_controller_0_lookup_valid_o;
+  wire [9:0]repair_count_o_0;
+  wire repair_full_o_0;
+  wire repair_reset_i_0;
+  wire [9:0]repaired_logical_addr_o_0;
+  wire [9:0]repaired_physical_addr_o_0;
+  wire repaired_valid_o_0;
+  wire rst_i;
+  wire start_i_0;
+  wire [0:0]xlconstant_0_dout;
+
+  Bit32_version_addr_gen_0_0 addr_gen_0
+       (.addr_dir_i(control_logic_0_addr_dir_o),
+        .addr_done_o(addr_gen_0_addr_done_o),
+        .addr_en_i(control_logic_0_addr_en_o),
+        .addr_o(addr_gen_0_addr_o),
+        .addr_rst_i(control_logic_0_addr_rst_o),
+        .clk_i(clk_i),
+        .rst_i(rst_i));
+  Bit32_version_address_remapper_0_0 address_remapper_0
+       (.logical_addr_i(addr_gen_0_addr_o),
+        .physical_addr_o(address_remapper_0_physical_addr_o),
+        .repair_addr_i(repair_controller_0_lookup_phys_addr_o),
+        .repair_valid_i(repair_controller_0_lookup_valid_o));
+  Bit32_version_blk_mem_gen_0_0 blk_mem_gen_0
+       (.addra(address_remapper_0_physical_addr_o),
+        .addrb(address_remapper_0_physical_addr_o),
+        .clka(clk_i),
+        .clkb(clk_i),
+        .dina(data_gen_0_data_o),
+        .doutb(blk_mem_gen_0_doutb),
+        .ena(xlconstant_0_dout),
+        .enb(xlconstant_0_dout),
+        .wea(control_logic_0_we_o));
+  Bit32_version_comparator_0_0 comparator_0
+       (.addr_i(addr_gen_0_addr_o),
+        .clk_i(clk_i),
+        .compare_en_i(control_logic_0_compare_en_o),
+        .expected_data_i(data_gen_0_data_o),
+        .fail_count_o(fail_count_o_0),
+        .fail_flag_o(fail_flag_o_0),
+        .fault_addr_o(fault_addr_o),
+        .fault_addr_valid_o(fault_addr_valid_o),
+        .read_data_i(fault_inject_0_data_o),
+        .rst_i(rst_i));
+  Bit32_version_control_logic_0_0 control_logic_0
+       (.addr_dir_o(control_logic_0_addr_dir_o),
+        .addr_done_i(addr_gen_0_addr_done_o),
+        .addr_en_o(control_logic_0_addr_en_o),
+        .addr_rst_o(control_logic_0_addr_rst_o),
+        .clk_i(clk_i),
+        .compare_en_o(control_logic_0_compare_en_o),
+        .done_o(done_o_0),
+        .pattern_sel_o(control_logic_0_pattern_sel_o),
+        .rst_i(rst_i),
+        .start_i(start_i_0),
+        .we_o(control_logic_0_we_o));
+  Bit32_version_data_gen_0_0 data_gen_0
+       (.data_o(data_gen_0_data_o),
+        .pattern_sel_i(control_logic_0_pattern_sel_o));
+  Bit32_version_fault_inject_0_0 fault_inject_0
+       (.addr_i(address_remapper_0_physical_addr_o),
+        .data_i(blk_mem_gen_0_doutb),
+        .data_o(fault_inject_0_data_o),
+        .fault_en_i(fault_en_i_0));
+  Bit32_version_repair_controller_0_0 repair_controller_0
+       (.clk_i(clk_i),
+        .fault_addr_i(fault_addr_o),
+        .fault_valid_i(fault_addr_valid_o),
+        .lookup_addr_i(addr_gen_0_addr_o),
+        .lookup_phys_addr_o(repair_controller_0_lookup_phys_addr_o),
+        .lookup_valid_o(repair_controller_0_lookup_valid_o),
+        .repair_count_o(repair_count_o_0),
+        .repair_full_o(repair_full_o_0),
+        .repair_reset_i(repair_reset_i_0),
+        .repaired_logical_addr_o(repaired_logical_addr_o_0),
+        .repaired_physical_addr_o(repaired_physical_addr_o_0),
+        .repaired_valid_o(repaired_valid_o_0),
+        .rst_i(rst_i));
+  Bit32_version_xlconstant_0_0 xlconstant_0
+       (.dout(xlconstant_0_dout));
+endmodule
---
+++ Codes/rtl/Bit32_version_wrapper.v
@@ -0,0 +1,77 @@
+//Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+//Copyright 2022-2025 Advanced Micro Devices, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2025.2 (win64) Build 6299465 Fri Nov 14 19:35:11 GMT 2025
+//Date        : Mon Aug 31 23:53:25 2026
+//Host        : HP15S running 64-bit major release  (build 9200)
+//Command     : generate_target Bit32_version_wrapper.bd
+//Design      : Bit32_version_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module Bit32_version_wrapper
+   (clk_i,
+    done_o_0,
+    fail_count_o_0,
+    fail_flag_o_0,
+    fault_addr_o,
+    fault_addr_valid_o,
+    fault_en_i_0,
+    repair_count_o_0,
+    repair_full_o_0,
+    repair_reset_i_0,
+    repaired_logical_addr_o_0,
+    repaired_physical_addr_o_0,
+    repaired_valid_o_0,
+    rst_i,
+    start_i_0);
+  input clk_i;
+  output done_o_0;
+  output [7:0]fail_count_o_0;
+  output fail_flag_o_0;
+  output [9:0]fault_addr_o;
+  output fault_addr_valid_o;
+  input fault_en_i_0;
+  output [9:0]repair_count_o_0;
+  output repair_full_o_0;
+  input repair_reset_i_0;
+  output [9:0]repaired_logical_addr_o_0;
+  output [9:0]repaired_physical_addr_o_0;
+  output repaired_valid_o_0;
+  input rst_i;
+  input start_i_0;
+
+  wire clk_i;
+  wire done_o_0;
+  wire [7:0]fail_count_o_0;
+  wire fail_flag_o_0;
+  wire [9:0]fault_addr_o;
+  wire fault_addr_valid_o;
+  wire fault_en_i_0;
+  wire [9:0]repair_count_o_0;
+  wire repair_full_o_0;
+  wire repair_reset_i_0;
+  wire [9:0]repaired_logical_addr_o_0;
+  wire [9:0]repaired_physical_addr_o_0;
+  wire repaired_valid_o_0;
+  wire rst_i;
+  wire start_i_0;
+
+  Bit32_version Bit32_version_i
+       (.clk_i(clk_i),
+        .done_o_0(done_o_0),
+        .fail_count_o_0(fail_count_o_0),
+        .fail_flag_o_0(fail_flag_o_0),
+        .fault_addr_o(fault_addr_o),
+        .fault_addr_valid_o(fault_addr_valid_o),
+        .fault_en_i_0(fault_en_i_0),
+        .repair_count_o_0(repair_count_o_0),
+        .repair_full_o_0(repair_full_o_0),
+        .repair_reset_i_0(repair_reset_i_0),
+        .repaired_logical_addr_o_0(repaired_logical_addr_o_0),
+        .repaired_physical_addr_o_0(repaired_physical_addr_o_0),
+        .repaired_valid_o_0(repaired_valid_o_0),
+        .rst_i(rst_i),
+        .start_i_0(start_i_0));
+endmodule
